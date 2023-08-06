3cfeef39e4adcba1f41271e2a50df96c68633336
Shelby Rothman <shelby.rothman@aderant.com>
2023-08-06 10:33:48 -0700

    allow support for web and fix url prefix

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { AppRegistry, Platform } from 'react-native'
 import App from './src/App'
 import { registerRootComponent } from 'expo';
 
-// AppRegistry.registerComponent('main', () => App)
 
 if (Platform.OS === 'web') {
+    AppRegistry.registerComponent('main', () => App)
     const rootTag = document.getElementById('root') || document.getElementById('X');
     AppRegistry.runApplication('main', { rootTag });
 }
